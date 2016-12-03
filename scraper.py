36a0868a0781260381acbb89a1166c7b66598d50
roshin8 <theroshin@gmail.com>
2016-12-03 12:00:52 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -43,5 +43,5 @@ for tr in proxy_table.tbody.find_all('tr'):
 			ip_address_text.append(item)
 
 	ip_address = ''.join(ip_address_text)
-	scraperwiki.sqlite.save(unique_keys=["IP_Address"], data={"IP_address": ip_address, "Anon": anon, "Type": connection_type, "Port": port, "Country": country,  "Last_Update": last_update})
+	scraperwiki.sqlite.save(unique_keys=["IP_Address"], data={"IP_address": ip_address, "Port": port,  "Last_Update": last_update, "Anon": anon, "Type": connection_type,  "Country": country})
   
